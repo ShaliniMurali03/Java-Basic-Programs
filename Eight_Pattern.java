bf68ad57b17ab3a592ea3084d4a9a26a54861f38
ShaliniMurali03 <117591791+ShaliniMurali03@users.noreply.github.com>
2022-11-13 19:27:06 +0530

    Create Eight_Pattern.java
    Version1.0

---
+++ Eight_Pattern.java
@@ -0,0 +1,23 @@
+public class Eight_Pattern
+{
+    public static void main(String[] args)
+    {
+        int n = 5;
+        int i , j;
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                if(i==0 || i==n-1 || i==n/2 || j==0 || j==n-1)
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
